7f9034fba26f5b151ca4074c6bd9e2c5312bdbda
riazenelaj <98044563+riazenelaj@users.noreply.github.com>
2025-08-03 10:46:45 +0200

    chore: initial commit with client and server folders

---
+++ kanban-board-server/src/main/java/ria/kanban_board_server/KanbanBoardServerApplication.java
@@ -0,0 +1,13 @@
+package ria.kanban_board_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanBoardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanBoardServerApplication.class, args);
+	}
+
+}
---
+++ kanban-board-server/src/test/java/ria/kanban_board_server/KanbanBoardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ria.kanban_board_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanBoardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
